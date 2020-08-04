5ea55f67262834e43041a15e5bf250386de7ca96
Baris <barisolgun02@gmail.com>
2020-08-04 22:42:50 +0300

    day 7

---
+++ day7/js-basics/index.js
@@ -0,0 +1,52 @@
+// let name = 'Baris';
+
+// Cannat be a reserved keyword
+// Should be meaningfull 
+// Cannat start with number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// let firstName = 'Baris',
+// lastName = 'Olgun';
+
+
+// interestRate = 1; //const error const's can not be changed
+
+// console.log(interestRate);
+
+// let age = 18;
+
+// let isApproved = true;
+
+// let 
+
+
+
+
+// const person = {
+//     name: 'Baris',
+//     lastname: 'Olgun',
+//     age: 18, 
+// };
+// person.name = 'John';
+
+// let selection = 'name';
+// person[selection] = 'Mary';
+
+// // console.log(person);
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+
+// console.log(selectedColors);
+
+
+// function greet(name, lastname) {
+//     console.log('welcome ' + name + (' ') + lastname);
+// }
+// greet('john', 'smith');
+
+function square(number) {
+    return number * number;
+}
+console.log(square(2));
