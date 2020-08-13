bf44a7aa2d008a84e54be650d3e63562d680ac0c
Baris <barisolgun02@gmail.com>
2020-08-13 20:13:33 +0300

    Fatih Acet Js_1

---
+++ day_15/main.js
@@ -0,0 +1,28 @@
+var Fatih = { //object
+    name: 'Fatih', //String primitive 
+    age: 31, //Number primitive 
+    isWorking: true //Boolean primitive 
+}
+console.log(typeof(Fatih))
+
+
+age = null;
+
+console.log(typeof age)
+
+console.log(Number.MAX_VALUE)
+
+// NAN  Not a number
+
+
+
+// 5000  number => integer
+
+// 55.12  Number =>Float
+
+
+var number_1 = 55.12;
+
+var number_2 = 12.15;
+
+console.log(parseInt(number_1 + number_2))
